1464b3bbaf2e2d938910744d8eeb3d3ecc0f3af3
huahai <279598616@qq.com>
2018-11-30 11:59:28 +0800

    izCompileConfig  izTypes

---
+++ IrrlichtZero/IrrlichtZero/izCompileConfig.h
@@ -0,0 +1,12 @@
+# pragma once
+
+// IrrlichtZero SDK Version
+#define IRRLICHTZERO_MAJOR 0
+#define IRRLICHTZERO_MINOR 0
+#define IRRLICHTZERO_REVISION 1
+
+#define IRRLICHTZERO_SDK_VERSION "0.0.1"
+
+#include<stdio.h>
+
+//eliminate unnecessary compile config,only for windows and software-rendering
---
+++ IrrlichtZero/IrrlichtZero/izTypes.h
@@ -0,0 +1,30 @@
+# pragma once
+
+#include"izCompileConfig.h"
+
+//iz is short for irrlichtzero
+namespace iz
+{
+	// 8 bit unsigned variable
+	typedef unsigned char   u8;
+	// 8 bit signed variable
+	typedef signed char		s8;
+	// 8 bit character variable
+	typedef char            c8;
+	// 16 bit unsigned variable
+	typedef unsigned short  u16;
+	// 16 bit signed variable
+	typedef signed short    s16;
+	// 32 bit unsigned variable
+	typedef unsigned int    u32;
+	// 32 bit signed variable
+	typedef signed int      s32;
+	// 64 bit unsigned variable
+	typedef unsigned long long u64;
+	// 64 bit signed variable
+	typedef signed long long s64;
+	// 32 bit floating point variable
+	typedef float           f32;
+	// 64 bit floating point variable
+	typedef double          f64;
+}//end namespace iz
